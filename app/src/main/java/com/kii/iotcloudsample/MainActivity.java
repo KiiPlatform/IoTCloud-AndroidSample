746a99cb6aeb6e480080903e192bb069e996b53b
satoshi <satoshi.kumano@kii.com>
2015-08-26 13:40:25 +0900

    Set tab title and removed unused codes.

--- app/src/main/java/com/kii/iotcloudsample/MainActivity.java
+++ app/src/main/java/com/kii/iotcloudsample/MainActivity.java
@@ -43,28 +43,23 @@ public class MainActivity extends AppCompatActivity {
             return 5;
         }
 
-//        @Override
-//        public boolean isViewFromObject(View view, Object o) {
-//            return o == view;
-//        }
-//
-//        @Override
-//        public CharSequence getPageTitle(int position) {
-//            switch(position) {
-//                case 0:
-//                    return "Onboard";
-//                case 1:
-//                    return "Commands";
-//                case 2:
-//                    return "Triggers";
-//                case 3:
-//                    return "States";
-//                case 4:
-//                    return "Info";
-//                default:
-//                    throw new RuntimeException("Unxepected flow.");
-//            }
-//        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position) {
+                case 0:
+                    return "Onboard";
+                case 1:
+                    return "Commands";
+                case 2:
+                    return "Triggers";
+                case 3:
+                    return "States";
+                case 4:
+                    return "Info";
+                default:
+                    throw new RuntimeException("Unxepected flow.");
+            }
+        }
 
         @Override
         public Fragment getItem(int position) {
@@ -83,42 +78,6 @@ public class MainActivity extends AppCompatActivity {
                     throw new RuntimeException("Unknown flow");
             }
         }
-
-//        @Override
-//        public Object instantiateItem(ViewGroup container, int position) {
-//            int layoutId = 0;
-//            switch (position) {
-//                case 0:
-//                    layoutId = R.layout.onboard_view;
-//                    break;
-//                case 1:
-//                    layoutId = R.layout.commands_view;
-//                    break;
-//                case 2:
-//                    layoutId = R.layout.triggers_view;
-//                    break;
-//                case 3:
-//                    layoutId = R.layout.states_view;
-//                    break;
-//                case 4:
-//                    layoutId = R.layout.info_view;
-//                    break;
-//                default:
-//                    throw new RuntimeException("Unknown flow");
-//            }
-//            // Inflate a new layout from our resources
-//            View view = MainActivity.this.getLayoutInflater().inflate(layoutId,
-//                    container, false);
-//            // Add the newly created View to the ViewPager
-//            container.addView(view);
-//
-//            return view;
-//        }
-//
-//        @Override
-//        public void destroyItem(ViewGroup container, int position, Object object) {
-//            container.removeView((View) object);
-//        }
 
     }
 
