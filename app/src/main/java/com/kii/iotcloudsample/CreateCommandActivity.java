44d00f666df50aec5838a2c7419275763a90adb8
satoshi <satoshi.kumano@kii.com>
2015-08-27 19:46:05 +0900

    Add Command Addition UI flow.

---
+++ app/src/main/java/com/kii/iotcloudsample/CreateCommandActivity.java
@@ -0,0 +1,18 @@
+package com.kii.iotcloudsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.kii.iotcloudsample.R;
+
+public class CreateCommandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_command);
+    }
+
+}
--- app/src/main/java/com/kii/iotcloudsample/fragments/CommandsFragment.java
+++ app/src/main/java/com/kii/iotcloudsample/fragments/CommandsFragment.java
@@ -1,12 +1,17 @@
 package com.kii.iotcloudsample.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.kii.iotcloudsample.CreateCommandActivity;
 import com.kii.iotcloudsample.R;
 
 /**
@@ -27,6 +32,15 @@ public class CommandsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.commands_view, null);
+        Button btnNewCommand = (Button) view.findViewById(R.id.buttonNewCommand);
+        btnNewCommand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setClass(getContext(), CreateCommandActivity.class);
+                getActivity().startActivity(i);
+            }
+        });
         return view;
     }
 
