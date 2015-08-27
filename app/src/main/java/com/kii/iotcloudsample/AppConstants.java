a3b9eabc434b00a6d25d79b02a4de7e9290b81b0
satoshi <satoshi.kumano@kii.com>
2015-08-27 12:31:01 +0900

    Removed AppConstants.APPSITE and unify with APPSITEURL

--- app/src/main/java/com/kii/iotcloudsample/AppConstants.java
+++ app/src/main/java/com/kii/iotcloudsample/AppConstants.java
@@ -6,5 +6,4 @@ public class AppConstants {
     public static final String APPID = "9ab34d8b";
     public static final String APPKEY = "7a950d78956ed39f3b0815f0f001b43b";
     public static final String APPSITEURL = "https://api-jp.kii.com/api";
-    public static final Site APPSITE = Site.JP;
 }
